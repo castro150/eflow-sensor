fe87a2c495e8d66aa10ff54e0c877b1a370b400c
castro150 <rafaelcarneiroget@hotmail.com>
2018-10-08 17:53:29 -0300

    POST do heroku service

--- src/main.cpp
+++ src/main.cpp
@@ -42,24 +42,25 @@ void loop() {
 
         USE_SERIAL.print("[HTTP] begin...\n");
         // configure traged server and url
-        http.begin("http://eflow-server.herokuapp.com/");
+        http.begin("http://eflow-server.herokuapp.com/users/");
 
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
+        USE_SERIAL.print("[HTTP] POST...\n");
+        // start connection and send HTTP request
+        http.addHeader("Content-Type", "application/json");
+        int httpCode = http.POST("{ \"param1\": 1, \"param2\": \"data string\" }");
 
         // httpCode will be negative on error
         if(httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
 
             if(httpCode == HTTP_CODE_OK) {
                 String payload = http.getString();
                 USE_SERIAL.println(payload);
             }
         } else {
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+            USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
 
         http.end();
