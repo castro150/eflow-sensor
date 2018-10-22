d8c93f13424a91ceb54b325688c127f3bb9f266a
castro150 <rafaelcarneiroget@hotmail.com>
2018-10-22 18:47:30 -0300

    Sending flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-/**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
- */
-
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
@@ -20,13 +13,15 @@ unsigned long previousMillis = 0;
 const long INTERVAL = 5000;
 
 const byte INTERRUPT_PIN = 13; // D7 - Node MCU ESP8266
-volatile uint32_t pulseCounter = 0;
-volatile uint64_t liters = 0;
+volatile float pulseCounter = 0;
+volatile float liters = 0;
+volatile float totalLiters = 0;
+volatile float flow = 0;
+
+// TODO remover logs
 
 void handleInterrupt() {
     pulseCounter++;
-    // TODO remover log
-    USE_SERIAL.printf("[INTERRUPT] count %d...\n", pulseCounter);
 }
 
 void setup() {
@@ -50,38 +45,54 @@ void loop() {
     unsigned long currentMillis = millis();
     
     // wait for WiFi connection
-    if((WiFiMulti.run() == WL_CONNECTED) && (currentMillis - previousMillis >= INTERVAL)) {
-        // TODO obter litros do contador de pulso, zerá-lo e somar aos litros atuais
-        // TODO calcular vazão
-        // TODO enviar tudo para servidor
-
+    if(currentMillis - previousMillis >= INTERVAL) {
+        liters = pulseCounter / 516;
+        USE_SERIAL.printf("[PULSE] count %f...\n", pulseCounter);
+        pulseCounter = 0;
+        flow = liters * 12;
+        totalLiters += liters;
+        // TODO remover logs
+        USE_SERIAL.printf("[FLOW] count %f...\n", flow);
+        USE_SERIAL.printf("[LITERS] count %f...\n", liters);
+        USE_SERIAL.printf("[TOTAL LITERS] count %f...\n", totalLiters);
         previousMillis = currentMillis;
-        HTTPClient http;
 
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        http.begin("http://eflow-server.herokuapp.com/users/");
+        if (WiFiMulti.run() == WL_CONNECTED) {
+            char buff[100];
+            char flowString[20];
+            dtostrf(flow, 4, 6, flowString);  // 4 is mininum width, 6 is precision
 
-        USE_SERIAL.print("[HTTP] POST...\n");
-        // start connection and send HTTP request
-        http.addHeader("Content-Type", "application/json");
-        int httpCode = http.POST("{ \"param1\": 1, \"param2\": \"data string\" }");
+            HTTPClient http;
 
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+            USE_SERIAL.print("[HTTP] begin...\n");
+            // configure traged server and url
+            http.begin("http://eflow-server.herokuapp.com/readings/");
 
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
+            USE_SERIAL.print("[HTTP] POST...\n");
+            // start connection and send HTTP request
+            http.addHeader("Content-Type", "application/json");
+            snprintf(buff, sizeof(buff), "{\"sensor\":\"5bc4f1ec82c009052c8d881b\",\"flow\":%s}", flowString);
+            USE_SERIAL.printf("[SENDING] ");
+            USE_SERIAL.printf(buff);
+            USE_SERIAL.printf("\n");
+            int httpCode = http.POST(buff);
+
+            // httpCode will be negative on error
+            if (httpCode > 0) {
+                // HTTP header has been send and Server response header has been handled
+                USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+
+                if(httpCode == HTTP_CODE_OK) {
+                    String payload = http.getString();
+                    USE_SERIAL.println(payload);
+                }
+            } else {
+                USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+                USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
             }
-        } else {
-            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
-            USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
 
-        http.end();
+            http.end();
+        }
     }
 }
 
