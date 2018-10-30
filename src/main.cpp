dded12fa6c7dc5bea3032ab54af290e4be25e35a
castro150 <rafaelcarneiroget@hotmail.com>
2018-10-30 17:03:53 -0300

    Sending consumption

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ESP8266WiFiMulti.h>
 
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define USE_SERIAL Serial
 
@@ -18,10 +19,39 @@ volatile float liters = 0;
 volatile float totalLiters = 0;
 volatile float flow = 0;
 
-// TODO remover logs
-
 void handleInterrupt() {
     pulseCounter++;
+}
+
+void updateActualConsumption() {
+    if (WiFiMulti.run() == WL_CONNECTED) {
+        HTTPClient http;
+
+        // configure traged server and url
+        http.begin("http://eflow-server.herokuapp.com/readings/actual/");
+
+        USE_SERIAL.print("[HTTP] GET consumption...\n");
+        // start connection and send HTTP request
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            USE_SERIAL.printf("[HTTP] GET code: %d\n", httpCode);
+
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                StaticJsonBuffer<300> JSONBuffer;
+                JsonObject& parsed = JSONBuffer.parseObject(payload);
+                totalLiters = parsed["consumption"];
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET code: %d, error: %s\n", httpCode, http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
 }
 
 void setup() {
@@ -29,16 +59,15 @@ void setup() {
     pinMode(INTERRUPT_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), handleInterrupt, RISING);
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
-
     WiFi.mode(WIFI_STA);
     WiFiMulti.addAP("CASA", "abcdef1234");
+    while (WiFiMulti.run() != WL_CONNECTED) {
+        USE_SERIAL.print(".");
+        delay(1000);
+    }
+    USE_SERIAL.print("\n");
 
-    // TODO buscar litros do servidor
+    updateActualConsumption();
 }
 
 void loop() {
@@ -47,48 +76,42 @@ void loop() {
     // wait for WiFi connection
     if(currentMillis - previousMillis >= INTERVAL) {
         liters = pulseCounter / 516;
-        USE_SERIAL.printf("[PULSE] count %f...\n", pulseCounter);
         pulseCounter = 0;
         flow = liters * 12;
         totalLiters += liters;
-        // TODO remover logs
-        USE_SERIAL.printf("[FLOW] count %f...\n", flow);
-        USE_SERIAL.printf("[LITERS] count %f...\n", liters);
-        USE_SERIAL.printf("[TOTAL LITERS] count %f...\n", totalLiters);
+        USE_SERIAL.printf("[FLOW] %f / [LITERS] %f / [TOTAL LITERS] %f\n", flow, liters, totalLiters);
         previousMillis = currentMillis;
 
         if (WiFiMulti.run() == WL_CONNECTED) {
             char buff[100];
+            char consumptionString[20];
             char flowString[20];
+            dtostrf(totalLiters, 4, 6, consumptionString);  // 4 is mininum width, 6 is precision
             dtostrf(flow, 4, 6, flowString);  // 4 is mininum width, 6 is precision
 
             HTTPClient http;
 
-            USE_SERIAL.print("[HTTP] begin...\n");
             // configure traged server and url
             http.begin("http://eflow-server.herokuapp.com/readings/");
 
-            USE_SERIAL.print("[HTTP] POST...\n");
+            USE_SERIAL.print("[HTTP] POST new reading...\n");
             // start connection and send HTTP request
             http.addHeader("Content-Type", "application/json");
-            snprintf(buff, sizeof(buff), "{\"sensor\":\"5bc4f1ec82c009052c8d881b\",\"flow\":%s}", flowString);
-            USE_SERIAL.printf("[SENDING] ");
-            USE_SERIAL.printf(buff);
-            USE_SERIAL.printf("\n");
+            snprintf(buff, sizeof(buff), "{\"sensor\":\"5bc4f1ec82c009052c8d881b\",\"consumption\":%s,\"flow\":%s}", consumptionString, flowString);
+            USE_SERIAL.printf("[SENDING] %s\n", buff);
             int httpCode = http.POST(buff);
 
             // httpCode will be negative on error
             if (httpCode > 0) {
                 // HTTP header has been send and Server response header has been handled
-                USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+                USE_SERIAL.printf("[HTTP] POST code: %d\n", httpCode);
 
                 if(httpCode == HTTP_CODE_OK) {
                     String payload = http.getString();
                     USE_SERIAL.println(payload);
                 }
             } else {
-                USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
-                USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+                USE_SERIAL.printf("[HTTP] POST code: %d, error: %s\n", httpCode, http.errorToString(httpCode).c_str());
             }
 
             http.end();
